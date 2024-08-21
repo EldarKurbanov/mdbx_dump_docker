4fe6b930aff17111df277b6f79ef53d20f815d5d
Eldar Kurbanov <EldarK@outlook.com>
2024-08-21 16:34:59 +0300

    add mdbx_load

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM gcc:14
+FROM gcc:14 AS mdbx_tools
 
 WORKDIR /root
 RUN git clone --depth 1 --branch v0.12.11 https://gitflic.ru/project/erthink/libmdbx.git
@@ -6,6 +6,16 @@ WORKDIR /root/libmdbx
 
 RUN make tools-static
 
-FROM scratch
-COPY --from=0 /root/libmdbx/mdbx_dump.static /bin/mdbx_dump
+FROM scratch AS mdbx_dump
+
+USER 1001:1001
+
+COPY --from=mdbx_tools /root/libmdbx/mdbx_dump.static /bin/mdbx_dump
 ENTRYPOINT ["/bin/mdbx_dump"]
+
+FROM scratch AS mdbx_load
+
+USER 1001:1001
+
+COPY --from=mdbx_tools /root/libmdbx/mdbx_load.static /bin/mdbx_load
+ENTRYPOINT ["/bin/mdbx_load"]
--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
-docker build -t eldarkurbanov/mdbx_dump:v0.12.11 -t eldarkurbanov/mdbx_dump --platform=linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x .
-docker push eldarkurbanov/mdbx_dump:v0.12.11
-docker push eldarkurbanov/mdbx_dump
+#!/bin/bash
+
+tag="v0.12.11"
+
+function build_push() {
+    mdbx_tool=$1
+
+    docker build -t eldarkurbanov/$mdbx_tool:$tag -t eldarkurbanov/$mdbx_tool --target $mdbx_tool --platform=linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x .
+    docker push eldarkurbanov/$mdbx_tool:$tag
+    docker push eldarkurbanov/$mdbx_tool
+}
+
+build_push "mdbx_dump"
+build_push "mdbx_load"
